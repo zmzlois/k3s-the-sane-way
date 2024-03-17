613dce85da8cd8d2312f625673836f844e1fd440
zmzlois <lois.z@comcord.vision>
2024-03-17 04:40:55 +0000

    append content from master node into token file

---
+++ hostname/master-london.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+source ../shared/utils.sh
+source ./.profile
+
+# set hostname
+sudo hostnamectl set-hostname "k8s-master"
+
+echo "Installing k3s..."
+curl -sfL https://get.k3s.io | sh -
+
+echo "Copying the kubeconfig file to the local machine..."
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+echo "Setting the kubeconfig file to the right permissions..."
+chmod 600 ~/.kube/config
+
+echo "Accesing token for worker nodes to join the cluster..."
+checkLineThenAppend "/var/lib/rancher/k3s/server/node-token" "./token"
